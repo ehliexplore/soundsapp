23d9e30164f9c1f7fcf8dcd61ee930d3188ad4bf
Luis Guilherme dos Santos Guirardelli <lgsguirardelli@gmail.com>
2023-10-26 20:14:10 -0300

    adding TLOP drums sounds

--- soundsapp.py
+++ soundsapp.py
@@ -9,30 +9,49 @@ class SoundApp(App):
         layout = BoxLayout(orientation='vertical')
 
         # Create buttons
-        button1 = Button(text='Play Sound 1')
-        button2 = Button(text='Play Sound 2')
+        button_kick = Button(text='Kick')
+        button_snare = Button(text='Snare')
+        button_hihat = Button(text='HiHat')
+        button_clap = Button(text='Clap')
 
         # Bind the buttons to their respective functions
-        button1.bind(on_release=self.play_sound1)
-        button2.bind(on_release=self.play_sound2)
+        button_kick.bind(on_release=self.play_kick)
+        button_snare.bind(on_release=self.play_snare)
+        button_hihat.bind(on_release=self.play_hihat)
+        button_clap.bind(on_release=self.play_clap)
 
         # Add the buttons to the layout
-        layout.add_widget(button1)
-        layout.add_widget(button2)
+        layout.add_widget(button_kick)
+        layout.add_widget(button_snare)
+        layout.add_widget(button_hihat)
+        layout.add_widget(button_clap)
+
+        # Bind keyboard keys to button actions
+        ...
 
         return layout
     
     
-    def play_sound1(self, instance):
-        sound = SoundLoader.load('sound1.mp3')
+    def play_kick(self, instance):
+        sound = SoundLoader.load('kick.wav')
         if sound:
             sound.play()
     
-
-    def play_sound2(self, instance):
-        sound = SoundLoader.load('sound2.mp3')
+    def play_snare(self, instance):
+        sound = SoundLoader.load('snare.wav')
         if sound:
             sound.play()
+
+    def play_hihat(self, instance):
+        sound = SoundLoader.load('hihat.wav')
+        if sound:
+            sound.play()
+    
+    def play_clap(self, instance):
+        sound = SoundLoader.load('clap.wav')
+        if sound:
+            sound.play()
+
 
 
 if __name__ == '__main__':
